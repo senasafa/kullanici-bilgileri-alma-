660b88c93e099b39c2590b00bc65fce36a6e56da
senasafa <senasafa8@gmail.com>
2024-11-20 23:02:48 +0300

    Add files via upload

---
+++ ad soyad ögrencino bilgileri alma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+    Console.Write("adınızı girin: ");
+string ad = Console.ReadLine();
+
+Console.Write("soyadınızı girin: ");
+
+string soyad = Console.ReadLine();
+
+Console.Write("ögrenci numaranızı girin: ");
+string ogrencino = Console.ReadLine();
+
+Console.WriteLine("\ngirdiginiz bilgiler: ");
+Console.WriteLine("ad:" +  ad );
+    Console.WriteLine("soyad:" + soyad);
+Console.WriteLine("öğrenci no:" + ogrencino );
+Console.ReadKey();
+
+
+
+
+
+
+
+
+     
+     
+  
